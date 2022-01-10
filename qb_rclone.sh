3ba2b080778706f3c67cc35ca3c784203422bfcb
LoveCany <admin@lovecany.me>
2022-01-11 01:42:34 +0800

    fix command error

--- qb_rclone.sh
+++ qb_rclone.sh
@@ -5,8 +5,8 @@ src=$1
 src=$src
 dst=gdrive:/buyvm  
 echo  {$src} >/root/test.log    
-if [-d "$src"]; then
+if [ -d "$src" ]; then
 rclone -v copy --ignore-existing "$src" "$dst/$2" --log-file=/root/rclone.log
-elif [-f "src"]; then
+elif [ -f "src" ]; then
 rclone -v copy --ignore-existing "$src" "$dst" --log-file=/root/rclone.log
 fi
