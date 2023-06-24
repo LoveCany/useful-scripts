81977a996e43b732783876e42f75abc27e987f90
LoveCany <admin@lovecany.me>
2023-06-24 22:21:29 +0900

    Create moegirl-adblock-detect-bypass.user.js

---
+++ chrome-scripts/moegirl-adblock-detect-bypass.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         Moegirl ADBlock Detection Bypass
+// @namespace    https://lovecany.me/
+// @version      0.1
+// @description  跳过萌娘百科的广告屏蔽器检测
+// @author       LoveCany
+// @match        https://*moegirl.org*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=moegirl.org
+// @grant        GM_addStyle
+// @run-at       document-idle
+// ==/UserScript==
+
+(() => {
+    let app;
+    let observer;
+    let throttle = (callback = () => {}, time = 300) => {
+      let timer = -1;
+
+      return (...args) => {
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+          callback(...args);
+        }, time);
+      };
+    };
+    let debounce = (callback = () => {}, time = 300) => {
+      let timer = -1;
+
+      return (...args) => {
+        if (timer > -1) {
+          return;
+        }
+
+        timer = setTimeout(() => {
+          callback(...args);
+          clearTimeout(timer);
+          timer = -1;
+        }, time);
+      };
+    };
+    let detectionBypass = () => {
+      if (document.querySelector('fc-ab-root')) {
+        document.querySelector('fc-ab-root').remove();
+        observer?.disconnect?.();
+      }
+    };
+
+    app = document.querySelector('body');
+    observer = new MutationObserver(debounce(detectionBypass, 150));
+    observer.observe(app, { childList: true, subtree: true });
+  })();
