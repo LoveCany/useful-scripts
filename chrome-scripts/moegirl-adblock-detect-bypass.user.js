1005ed1dcd4fb38079bddf7351931aa22fa7baad
LoveCany <admin@lovecany.me>
2023-06-24 22:36:30 +0900

    maybe works now

--- chrome-scripts/moegirl-adblock-detect-bypass.user.js
+++ chrome-scripts/moegirl-adblock-detect-bypass.user.js
@@ -4,49 +4,19 @@
 // @version      0.1
 // @description  跳过萌娘百科的广告屏蔽器检测
 // @author       LoveCany
-// @match        https://*moegirl.org*/*
+// @match        https://zh.moegirl.org.cn/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=moegirl.org
 // @grant        GM_addStyle
 // @run-at       document-idle
 // ==/UserScript==
 
 (() => {
-  let app;
-  let observer;
-  let throttle = (callback = () => {}, time = 300) => {
-    let timer = -1;
-
-    return (...args) => {
-      clearTimeout(timer);
-
-      timer = setTimeout(() => {
-        callback(...args);
-      }, time);
-    };
-  };
-  let debounce = (callback = () => {}, time = 300) => {
-    let timer = -1;
-
-    return (...args) => {
-      if (timer > -1) {
-        return;
-      }
-
-      timer = setTimeout(() => {
-        callback(...args);
-        clearTimeout(timer);
-        timer = -1;
-      }, time);
-    };
-  };
-  let detectionBypass = () => {
-    if (document.getElementsByClassName('fc-ab-root')) {
-      document.getElementsByClassName('fc-ab-root')[0].remove()
-      observer?.disconnect?.();
+  let  observer = new MutationObserver(function(mutations) {
+    const e = document.getElementById('fc-ab-root');
+    if (e) {
+      e.remove();
+      this.disconnect();
     }
-  };
-
-  app = document.querySelector('body');
-  observer = new MutationObserver(debounce(detectionBypass, 150));
-  observer.observe(app, { childList: true, subtree: true });
+  });
+  observer.observe(document, { childList: true, subtree: true });
 })();
