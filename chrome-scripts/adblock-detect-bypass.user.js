b32be22f3eccdc2145c359c59c917a9c4f0fabea
LoveCany <admin@lovecany.me>
2023-06-26 20:25:49 +0900

    rename adblock-bypass to support more websites

--- chrome-scripts/adblock-detect-bypass.user.js
+++ chrome-scripts/adblock-detect-bypass.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         Moegirl ADBlock Detection Bypass
+// @name         ADBlock Detection Bypass
 // @namespace    https://lovecany.me/
 // @version      0.1
-// @description  跳过萌娘百科的广告屏蔽器检测
+// @description  跳过广告屏蔽器检测
 // @author       LoveCany
-// @match        https://zh.moegirl.org.cn/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=moegirl.org
+// @match        https://*.moegirl.org.cn/*
+// @match        https://*.op.gg/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=adblockplus.org
 // ==/UserScript==
 
 (() => {
