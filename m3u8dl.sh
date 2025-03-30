fe323a7c75af1169839718f860d8c94bde63530d
MEMORiA <admin@lovecany.me>
2025-03-30 21:46:30 +0900

    optimize command generation

--- m3u8dl.sh
+++ m3u8dl.sh
@@ -47,9 +47,14 @@ if [ -z "$INPUT_PATH" ]; then
     exit 1
 fi
 
-# If decryption key is given, use shaka packager to decrypt the video
-if [ -n "$DECRYPT_KEY" ]; then
-    N_m3u8DL-RE ${INPUT_PATH} --save-name $OUTPUT_NAME --binary-merge --no-date-info --decryption-engine $DECRYPTION_ENGINE --key $DECRYPT_KEY
-else
-    N_m3u8DL-RE ${INPUT_PATH} --save-name $OUTPUT_NAME --binary-merge --no-date-info
+EXECUTE_CMD="N_m3u8DL-RE ${INPUT_PATH}"
+if [ -n "$OUTPUT_NAME" ]; then
+    EXECUTE_CMD="${EXECUTE_CMD} --save-name ${OUTPUT_NAME}"
 fi
+if [ -n "$DECRYPT_KEY" ]; then
+    EXECUTE_CMD="${EXECUTE_CMD} --decryption-engine ${DECRYPTION_ENGINE} --key ${DECRYPT_KEY}"
+fi
+
+EXECUTE_CMD="${EXECUTE_CMD} --binary-merge --no-date-info"
+
+eval $EXECUTE_CMD
