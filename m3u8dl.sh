6df37e75b77d7d862b3f590f6090e41079aac76e
MEMORiA <admin@lovecany.me>
2025-03-30 21:59:17 +0900

    use getopts to solve dependency problems in MacOS

--- m3u8dl.sh
+++ m3u8dl.sh
@@ -19,25 +19,13 @@ Options:
     echo "$USAGE"
 }
 
-# For MacOS users, you may need to install gnu-getopt as original getopt is based on BSD rather than GNU
-# brew install gnu-getopt
-# export PATH="/opt/homebrew/opt/gnu-getopt/bin:$PATH"
-ARGS=$(getopt -o i:o:k:h --long input:,output:,key:,help -- "$@")
-VALID_ARGS=$?
-if [ $VALID_ARGS != "0" ]; then
-    usage;
-    exit 1
-fi
-
-eval set -- "$ARGS"
-while true; do
-    case "$1" in
-        -i|--input) INPUT_PATH="$2"; shift 2;;
-        -o|--output) OUTPUT_NAME="$2"; shift 2;;
-        -k|--key) DECRYPT_KEY="$2"; shift 2;;
-        -h|--help) usage; exit 0;;
-        --) shift; break;;
-        *) echo "Invalid option: ${2}"; usage; exit 1;;
+OPTSTRING="i:o:k:h"
+while getopts "$OPTSTRING" opt; do
+    case "$opt" in
+        i) INPUT_PATH=${OPTARG};;
+        o) OUTPUT_NAME=${OPTARG};;
+        k) DECRYPT_KEY=${OPTARG};;
+        h) usage; exit 0;;
     esac
 done
 
@@ -47,11 +35,11 @@ if [ -z "$INPUT_PATH" ]; then
     exit 1
 fi
 
-EXECUTE_CMD="N_m3u8DL-RE ${INPUT_PATH}"
-if [ -n "$OUTPUT_NAME" ]; then
-    EXECUTE_CMD="${EXECUTE_CMD} --save-name ${OUTPUT_NAME}"
+EXECUTE_CMD="N_m3u8DL-RE '${INPUT_PATH}'"
+if [ -n "${OUTPUT_NAME}" ]; then
+    EXECUTE_CMD="${EXECUTE_CMD} --save-name '${OUTPUT_NAME}'"
 fi
-if [ -n "$DECRYPT_KEY" ]; then
+if [ -n "${DECRYPT_KEY}" ]; then
     EXECUTE_CMD="${EXECUTE_CMD} --decryption-engine ${DECRYPTION_ENGINE} --key ${DECRYPT_KEY}"
 fi
 
